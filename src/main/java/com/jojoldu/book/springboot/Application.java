5cc85bc3ae8991e5abc63e5aff257791359fcfb9
cjsxorud111 <cjsxorud111@gmail.com>
2022-09-15 17:02:55 +0900

    save 기능추가

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,7 +4,6 @@ package com.jojoldu.book.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// 다른컴퓨터GITPUSH TEST
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
