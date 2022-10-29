7bd1b6dcacb4c3022f9161f050b6509722217fd6
cjsxorud111 <cjsxorud111@gmail.com>
2022-10-30 00:28:41 +0900

    더미데이터 스키마 생성용 테스트쿼리파일 추가

---
+++ src/main/resources/h2/data.sql
@@ -0,0 +1,16 @@
+insert into posts
+(
+    created_date,
+    modified_date,
+    author,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '태경',
+    'test',
+    '테스트제목'
+);
