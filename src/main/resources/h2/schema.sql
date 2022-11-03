f4b465c7a9fc3e809cfe926cc7ee7191699163aa
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-03 09:00:10 +0900

    한입 웹사이트 개발을 위한 개발용 자동생성 쿼리 추가

--- src/main/resources/h2/schema.sql
+++ src/main/resources/h2/schema.sql
@@ -1,18 +1,4 @@
-drop table posts if exists;
-
-create table posts
-(
-    id bigint not null auto_increment,
-    created_date datetime,
-    modified_date datetime,
-    author varchar(255),
-    content clob not null,
-    title varchar(500) not null,
-    primary key (id)
-);
-
 drop table if exists user;
-
 create table user
 (
     id bigint not null auto_increment,
@@ -24,3 +10,52 @@ create table user
     role varchar(255) not null,
     primary key (id)
 );
+
+drop table posts if exists;
+create table posts
+(
+    id bigint not null auto_increment,
+    created_date datetime,
+    modified_date datetime,
+    author varchar(255),
+    content clob not null,
+    title varchar(500) not null,
+    primary key (id)
+);
+
+drop table program if exists;
+create table program
+(
+    id bigint not null auto_increment,
+    created_date datetime,
+    modified_date datetime,
+    author varchar(255),
+    content clob not null,
+    title varchar(500) not null,
+    primary key (id)
+);
+
+drop table reservation if exists;
+create table reservation
+(
+    id bigint not null auto_increment,
+    created_date datetime,
+    modified_date datetime,
+    author varchar(255),
+    content clob not null,
+    title varchar(500) not null,
+    primary key (id)
+);
+
+drop table qna if exists;
+create table qna
+(
+    id bigint not null auto_increment,
+    created_date datetime,
+    modified_date datetime,
+    author varchar(255),
+    content clob not null,
+    title varchar(500) not null,
+    primary key (id)
+);
+
