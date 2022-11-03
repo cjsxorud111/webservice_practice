dcc85fa5265c9ca7b4cffcb65d25581143c80595
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-03 09:27:49 +0900

    프로그램 더미데이터 쿼리추가

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -14,3 +14,35 @@ values
     'test',
     '테스트제목'
 );
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
