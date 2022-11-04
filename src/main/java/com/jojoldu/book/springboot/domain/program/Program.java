f5a60c1198e2b5ffc8429a55babe6eb73d159452
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-04 09:40:49 +0900

    프로그램 엔티티추가

---
+++ src/main/java/com/jojoldu/book/springboot/domain/program/Program.java
@@ -0,0 +1,37 @@
+package com.jojoldu.book.springboot.domain.program;
+
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Program extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "clob", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Program(String title, String content, String author) { //생성자
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
