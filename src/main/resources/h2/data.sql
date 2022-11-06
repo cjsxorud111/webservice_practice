b350ad40828914ca76ef20da73472ad51b793092
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-06 09:15:46 +0900

    정보페이지 더미텍스트추가

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -14,11 +14,13 @@ values
     'test',
     '테스트제목'
 );
+--프로그램더미데이터
 insert into program
 (
     created_date,
     modified_date,
     author,
+    author_id,
     content,
     title
 )
@@ -27,6 +29,7 @@ values
     '2022-10-23 22:58:58.034',
     '2022-10-23 22:58:58.034',
     '프로그램테스트작성자0',
+    1,
     '프로그램테스트컨텐츠0',
     '프로그램테스트제목0'
 );
@@ -35,6 +38,7 @@ insert into program
     created_date,
     modified_date,
     author,
+    author_id,
     content,
     title
 )
@@ -43,6 +47,619 @@ values
     '2022-10-23 22:58:58.034',
     '2022-10-23 22:58:58.034',
     '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
+    '프로그램테스트컨텐츠1',
+    '프로그램테스트제목1'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자0',
+    1,
+    '프로그램테스트컨텐츠0',
+    '프로그램테스트제목0'
+);
+insert into program
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    '프로그램테스트작성자1',
+    1,
     '프로그램테스트컨텐츠1',
     '프로그램테스트제목1'
 );
