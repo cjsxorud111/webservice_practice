06aa033c2851617ba246b2afd0e255b81ebbccc5
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-07 09:14:00 +0900

    프로그램저장시 작성자id dto추가

--- src/main/java/com/jojoldu/book/springboot/web/dto/ProgramSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/ProgramSaveRequestDto.java
@@ -4,19 +4,23 @@ import com.jojoldu.book.springboot.domain.program.Program;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ProgramSaveRequestDto {
     private String title;
     private String content;
     private String author;
+    private Long authorId;
 
     @Builder
-    public ProgramSaveRequestDto(String title, String content, String author) {
+    public ProgramSaveRequestDto(String title, String content, String author, Long authorId) {
         this.title = title;
         this.content = content;
         this.author = author;
+        this.authorId = authorId;
     }
 
     public Program toEntity() {
@@ -24,6 +28,7 @@ public class ProgramSaveRequestDto {
                 .title(title)
                 .content(content)
                 .author(author)
+                .userId(authorId)
                 .build();
     }
 }
