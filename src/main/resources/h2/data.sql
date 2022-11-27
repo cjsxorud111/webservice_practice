f5c3dee52a4f982813904044e98c0b9f3fd2a60f
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-27 22:47:09 +0900

    qna데이터 추가

--- src/main/resources/h2/data.sql
+++ src/main/resources/h2/data.sql
@@ -1,3 +1,230 @@
+insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자2',
+    2,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);
+insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자0',
+    1,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);
+insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자0',
+    1,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);
+insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자0',
+    1,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);
+insert into qna
+(
+    created_date,
+    modified_date,
+    author,
+    author_id,
+    content,
+    title
+)
+values
+(
+    '2022-10-23 22:58:58.034',
+    '2022-10-23 22:58:58.034',
+    'qna테스트작성자0',
+    1,
+    'qna테스트컨텐츠0',
+    'qna테스트제목0'
+);
+
 insert into posts
 (
     created_date,
