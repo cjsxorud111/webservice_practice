fcf7c976f99d88bfdf965a6ec3555c927a146140
cjsxorud111 <cjsxorud111@gmail.com>
2022-11-30 22:03:39 +0900

    QnaServiceTest 추가 findAllDesc 테스트 추가

---
+++ src/test/java/com/jojoldu/book/springboot/service/qna/QnaServiceTest.java
@@ -0,0 +1,57 @@
+package com.jojoldu.book.springboot.service.qna;
+
+import com.jojoldu.book.springboot.domain.qna.Qna;
+import com.jojoldu.book.springboot.domain.qna.QnaRepository;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class QnaServiceTest {
+
+    @Mock
+    private QnaRepository qnaRepository;
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void findAllDesc() {
+        //given
+        Pageable pageable = mock(Pageable.class);
+        QnaService mockQnaService = mock(QnaService.class);
+        Qna qna = Qna.builder()
+                .title("qna테스트제목0")
+                .content("qna테스트컨텐츠0")
+                .author("qna테스트작성자0")
+                .userId(1L)
+                .build();
+
+        qna.setId(13L);
+        List<Qna> qnaList = new ArrayList<>();
+        qnaList.add(qna);
+        Page<Qna> pagenatedQnaList = new PageImpl<>(qnaList);
+        when(mockQnaService.findAllDesc(pageable)).thenReturn(pagenatedQnaList);
+
+        //when
+        Page<Qna> pagenatedQnaListForTest = mockQnaService.findAllDesc(pageable);
+
+        //then
+        assertThat(pagenatedQnaListForTest.getContent()).isEqualTo(qnaList);
+    }
+
+    @Test
+    public void delete() {
+    }
+}
