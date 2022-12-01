da05ce5b46477cf372f8f6f50acdf52304b0da84
cjsxorud111 <cjsxorud111@gmail.com>
2022-12-01 23:08:24 +0900

    ProgramServiceTest 추가 findById테스트() 생성

---
+++ src/test/java/com/jojoldu/book/springboot/service/program/ProgramServiceTest.java
@@ -0,0 +1,79 @@
+package com.jojoldu.book.springboot.service.program;
+
+import com.jojoldu.book.springboot.domain.program.Program;
+import com.jojoldu.book.springboot.domain.program.ProgramRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class ProgramServiceTest {
+    private ProgramService mockProgramService = mock(ProgramService.class);
+    private ProgramRepository mockProgramRepository = mock(ProgramRepository.class);
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findById테스트() {
+        //given
+        Long id = 1L;
+        Program program = Program.builder()
+                .title("프로그램제목0")
+                .content("프로그램컨텐츠0")
+                .author("프로그램작성자0")
+                .userId(1L)
+                .build();
+        program.setId(id);
+
+        when(mockProgramRepository.findById(id)).thenReturn(Optional.ofNullable(program));
+
+        //when
+        Program entity = mockProgramRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당사용자가없습니다 id=" + id));
+
+        //then
+        assertThat(entity).isEqualTo(program);
+    }
+
+    @Test
+    void findAllDesc테스트() {
+        //given
+        Pageable pageable = mock(Pageable.class);
+        Program program = Program.builder()
+                .title("프로그램제목0")
+                .content("프로그램컨텐츠0")
+                .author("프로그램작성자0")
+                .userId(1L)
+                .build();
+        program.setId(13L);
+
+        List<Program> programList = new ArrayList<>();
+        programList.add(program);
+        Page<Program> pagenatedProgramList = new PageImpl<>(programList);
+        when(mockProgramService.findAllDesc(pageable)).thenReturn(pagenatedProgramList);
+
+        //when
+        Page<Program> pagenatedProgramListForTest = mockProgramService.findAllDesc(pageable);
+
+        //then
+        assertThat(pagenatedProgramListForTest.getContent()).isEqualTo(programList);
+    }
+
+    @Test
+    void delete() {
+    }
+}
